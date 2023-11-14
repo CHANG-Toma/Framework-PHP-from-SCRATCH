9234df3271300e2c3a3810d8aad7acf5d83e78c2
Toma Chang <toma11chang@gmail.com>
2023-11-14 15:49:56 +0100

    ajout de la connexion à la bdd

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Projet de création d'un framework from scratch !
    
+Adresse local phpMyAdmin : http://localhost:8181/
    
    
-   
-   docker compose build
+    docker compose build
 
 
     docker compose up
--- www/Core/View.php
+++ www/Core/View.php
@@ -4,15 +4,20 @@ namespace App\Core;
 
 Class View
 {
+    private $template;
     public function __construct($path, $layout)
     {
         if (file_exists("Views/" . $path . ".view.php")) {
             $path = "Views/" . $path . ".view.php";
-            include "Views/Templates/".$layout.".tpl.php";
+            $this->template =  "Views/Templates/".$layout.".tpl.php";
 
         } else {
             echo "pas de fichier";
         }
+    }
+
+    public function __destruct(){
+        include $this->template;
     }
 }
 
