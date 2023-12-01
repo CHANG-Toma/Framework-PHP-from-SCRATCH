6e3aff79f38065d45d4fc89906337446bffad07f
Toma Chang <toma11chang@gmail.com>
2023-12-01 09:55:27 +0100

    add : comment

--- www/Controllers/Security.php
+++ www/Controllers/Security.php
@@ -1,39 +1,52 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Core\View;
 use App\Models\User;
 
-class Security{
-
+class Security
+{
+    /**
+     * Gère la fonctionnalité de connexion de l'utilisateur.
+     */
     public function login(): void
     {
-        //Insertion d'un utilisateur
+        // Insère un nouvel utilisateur
         $myUser = new User();
         $myUser->setFirstname("toto");
-        $myUser->setLastname("   ChAnG    ");
+        $myUser->setLastname("   tata    ");
         $myUser->setEmail("toma11chang@gmail.com");
         $myUser->setPwd("eeee");
-        //$myUser->save();
+        //$myUser->setId(5);
+        $myUser->save();
 
-        //Modification d'un utilisateur
-        //récupère toutes les données de ce user
-        $myUser = User::populate(2);
-        if(!empty($myUser)){
+        // Modifie un utilisateur existant
+        $myUser = User::populate(5);
+        if (!empty($myUser)) {
             $myUser->setFirstname("Test");
             $myUser->save();
         }
 
+        // Affiche la vue "Security/login" dans le contexte "back"
         new View("Security/login", "back");
     }
+
+    /**
+     * Gère la fonctionnalité de déconnexion de l'utilisateur.
+     */
     public function logout(): void
     {
+        // Affiche le message "Logout"
         echo "Logout";
     }
+
+    /**
+     * Gère la fonctionnalité d'inscription de l'utilisateur.
+     */
     public function register(): void
     {
+        // Affiche le message "Register"
         echo "Register";
     }
-
-
 }
--- www/Core/DB.php
+++ www/Core/DB.php
@@ -7,57 +7,83 @@ class DB
     private $pdo;
     private $prefix = "esgi_";
     private $table;
+
+    /**
+     * Constructeur de la classe DB.
+     * Gère la connexion à la base de données et définit le nom de la table en fonction du nom de la classe.
+     */
     public function __construct()
     {
-        //Connexion à la bdd
-        try{
+        // Connexion à la base de données
+        try {
             $this->pdo = new \PDO("mysql:host=mariadb;dbname=esgi;charset=utf8", "esgi", "esgipwd");
-        }catch (\PDOException $exception){
-            echo "Erreur de connexion à la base de données : ".$exception->getMessage();
+        } catch (\PDOException $exception) {
+            echo "Erreur de connexion à la base de données : " . $exception->getMessage();
         }
+
+        // Détermine le nom de la table en fonction du nom de la classe
         $table = get_called_class();
         $table = explode("\\", $table);
         $table = array_pop($table);
-        $this->table = $this->prefix.strtolower($table);
+        $this->table = $this->prefix . strtolower($table);
     }
 
-    public function getChlidVars(): array
+    /**
+     * Obtient les variables enfant de l'objet.
+     * @return array
+     */
+    public function getChildVars(): array
     {
+        // Récupère les variables de l'objet qui ne sont pas dans la classe parente
         $vars = array_diff_key(get_object_vars($this), get_class_vars(get_class()));
         return $vars;
     }
+
+    /**
+     * Enregistre l'objet dans la base de données en créant et exécutant une requête SQL d'insertion ou de mise à jour.
+     */
     public function save(): void
     {
-        //Création et execution d'une requête insert SQL
-        $childVars = $this->getChlidVars();
-        var_dump($childVars);
-        if (empty($this->getId())) { //si c'est vide
+        // Crée et exécute une requête SQL d'insertion ou de mise à jour
+        $childVars = $this->getChildVars();
 
-            $sql = "INSERT INTO ".$this->table." (".implode(", ", array_keys($childVars)).")
-            VALUES (:".implode(", :", array_keys($childVars)).")";
-        }
-        else { //sinon on update
-
-            $sql = "UPDATE ".$this->table." SET ";
-            foreach ($childVars as $key => $value){
-                $sql .= $key."=:".$key.", ";
+        if (empty($this->getId())) { // Si l'ID est vide, effectue une insertion
+            $sql = "INSERT INTO " . $this->table . " (" . implode(", ", array_keys($childVars)) . ")
+            VALUES (:" . implode(", :", array_keys($childVars)) . ")";
+        } else { // Sinon, effectue une mise à jour
+            $sql = "UPDATE " . $this->table . " SET ";
+            foreach ($childVars as $key => $value) {
+                $sql .= $key . "=:" . $key . ", ";
             }
             $sql = substr($sql, 0, -2);
             $sql .= " WHERE id=:id";
             $childVars["id"] = $this->getId();
         }
+
         $query = $this->pdo->prepare($sql);
         $query->execute($childVars);
-
     }
 
+    /**
+     * Peuple un objet avec des données de la base de données en fonction de son ID.
+     * @param $id
+     * @return object|int
+     */
     public static function populate($id): object|int
     {
+        // Récupère un objet par son ID
         return (new static())->getOneBy(["id" => $id], "object");
     }
 
+    /**
+     * Récupère un enregistrement de la base de données en fonction des critères spécifiés.
+     * @param array $data
+     * @param string $return
+     * @return object|array|int
+     */
     public function getOneBy(array $data, $return = "array"): object|array|int
     {
+        // Récupère un enregistrement en fonction des données fournies
         $sql = "SELECT * FROM " . $this->table . " WHERE ";
         foreach ($data as $key => $value) {
             $sql .= $key . "=:" . $key . " AND ";
@@ -65,7 +91,9 @@ class DB
         $sql = substr($sql, 0, -5);
         $query = $this->pdo->prepare($sql);
         $query->execute($data);
-        if($return == "object")
+
+        // Retourne le résultat sous forme d'objet, tableau ou entier en fonction de $return
+        if ($return == "object")
             $query->setFetchMode(\PDO::FETCH_CLASS, get_called_class());
         return $query->fetch();
     }
